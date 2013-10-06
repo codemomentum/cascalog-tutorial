c85009a92f586104674853b1d801df5ceedca7ef
Halit Olali <halit@blackgate.local>
2013-10-06 12:39:16 +0300

    samples

--- src/cascalog_tutorial/core.clj
+++ src/cascalog_tutorial/core.clj
@@ -12,9 +12,7 @@
         source (hfs-textline dir)]
     (<- outargs (source ?line) (c/re-parse [#"[^\s]+,"] ?line :>> outargs) (:distinct false))))
 
-(defn mahout-data [dir] (textline-parsed dir 3))
-
-(defn compute-count [output-tap in-dir]
+(defn count-ratings [output-tap in-dir]
     (?<- output-tap 
     	[?count] 
     	((hfs-delimited in-dir :delimiter ",") ?user ?item ?preference _)
@@ -24,7 +22,7 @@
 
 (defn movies-joined [output-tap ratings-dir movies-dir]
     (?<- output-tap 
-    	[?user ?preference ?name] 
+    	[?user ?preference ?name ?category] 
     	((hfs-delimited ratings-dir :delimiter ",") ?user ?item ?preference _)
     	((hfs-delimited movies-dir :delimiter "::") ?item ?name ?category)
     	;(:trap (stdout))
@@ -32,5 +30,39 @@
 	)
 	)
 
+(defn average-rating-for-each-movie [output-tap ratings-dir movies-dir]
+    (?<- output-tap 
+    	[?name ?avg ?count] 
+    	((hfs-delimited ratings-dir :delimiter ",") ?user ?item ?preference _)
+    	((hfs-delimited movies-dir :delimiter "::") ?item ?name ?category)
+    	;(:trap (stdout))
+    	(read-string ?preference :> ?pref)
+		(c/count ?count)
+		(c/sum ?pref :> ?sum)
+   		(div ?sum ?count :> ?avg)
+	)
+	)
+
+(defmapcatop split-category [cat-str] 
+	(seq (clojure.string/split cat-str #"\|"))
+)
+
+(defn lowercase [w] (.toLowerCase w))
+
+(defn average-rating-for-each-category [output-tap ratings-dir movies-dir]
+    (?<- output-tap 
+    	[?cat ?count ?avg] 
+    	((hfs-delimited ratings-dir :delimiter ",") ?user ?item ?preference _)
+    	((hfs-delimited movies-dir :delimiter "::") ?item ?name ?category)
+    	;(:trap (stdout))
+    	(read-string ?preference :> ?pref)
+    	(split-category ?category :> ?CAT)
+    	(lowercase ?CAT :> ?cat)
+		(c/count ?count)
+		(c/sum ?pref :> ?sum)
+        (div ?sum ?count :> ?avg)
+	)
+	)
+
 (defn -main [in-dir output-dir]
-  (compute-count (hfs-textline output-dir) in-dir))
+  (count-ratings (hfs-textline output-dir) in-dir))
